67b5aad30468616a6ead7b24c1ffc540dc3386e2
yamone178 <yamonemyatmoe1592@gmail.com>
2023-01-07 13:00:36 +0630

    button-test

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import pf from '../pf.jpg'
+import {AiOutlineArrowDown} from 'react-icons/ai'
 
 const HeroSection = () => {
   return (
@@ -9,11 +10,17 @@ const HeroSection = () => {
 <div className="col-span-6 ">
 
   <div className="">
-    <div className="md:w-[500px] ">
+    <div className="md:w-[500px] mb-8">
+            <p className='dark:text-[#ccc] font-bold tracking-wider text-[15px] mb-3'>Welcome To My World!</p>
             <p className=' dark:text-white font-bold text-[40px] mb-3'>I'm Yamone Myat Moe, <br /> A Web Developer</p>
             <p className=' dark:text-[#eee] font-normal text-[20px] '>My main focus is to bring a standard website  to the user</p>
     
-        </div>
+    </div>
+
+    <a className="dark:text-white border-2 border-pink-600  p-3 mt-4 flex w-[230px] justify-around items-center rounded-md hover:bg-pink-700 duration-150 hover:text-white group">
+     <span>  MORE ABOUT ME </span>
+      <span className='bg-pink-600 rounded-full w-[30px] h-[30px] flex items-center font-bold group-hover:bg-white  '><AiOutlineArrowDown className='m-auto text-white group-hover:text-black' /></span>
+    </a>
   </div>
 
 </div>
